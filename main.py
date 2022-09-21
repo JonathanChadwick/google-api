ae4ca551a46ce9a968d76645d02e6d66423fecf8
Jonathan <jon2043@gmail.com>
2022-09-21 07:44:33 -0700

    Completed draft of dictionary function

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import os
+import requests
 
-
-def review_to_dict(path): #---> dict
+KEYS = ['title', 'name', 'date', 'feedback']
+URL = 'http://<corpweb-external-IP>/feedback'
+def review_to_dict(path):
+    list_of_feedback = []
     for review_txt in os.listdir(path):
-        with open(review_txt) as file:
+        with open(path + review_txt) as file:
+            feedback_dict = {
+                'title' : '',
+                'name' : '',
+                'date' : '',
+                'feedback' : ''
+            }
             for i, line in enumerate(file):
-                #create list of dictionary titles to match with i
-                pass
+                if i >= 3:
+                    feedback_dict['feedback'] += line
+                else:
+                    feedback_dict[KEYS[i]] = line.strip()
+            list_of_feedback.append(feedback_dict)
+    return list_of_feedback
+
+def post_feedback(list_of_feedback):
+    for feedback_dict in list_of_feedback:
+        response = requests.post(URL , data=feedback_dict)
+        print(f"status: {response.status()}")
+
+
+
+
+
+#print(review_to_dict('reviews/'))
